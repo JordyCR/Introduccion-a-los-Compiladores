baf465884158723feac5efdf58c81d1b504df18b
JordyC++n <jcuan4@gmail.com>
2015-11-01 08:12:57 -0600

    Con atributos

--- 3er proyecto/html_tags.py
+++ 3er proyecto/html_tags.py
@@ -1,16 +1,39 @@
+# -*- coding: utf-8 -*-
 from bs4 import BeautifulSoup
 
 soup = BeautifulSoup(open("master.html"), "html5lib")
 
 tags = []
 
-for hijo in soup.find_all(True):
+
+# Se hace dos veces el recorrido (se puede evitar, está así por legibilidad)
+# El primero para encontrar todos los TAGS
+master = soup.find_all(True)
+for hijo in master:
     #print hijo.name
     tags.append(hijo.name)
 
 unicos = set(tags)
 
-print len(unicos)
-print
+# print len(unicos)
+# print
+# for u in unicos:
+#     print u
+
+# Pero antes creamos un hash con 'unicos' (será un dict de sets)
+ntags = {}
 for u in unicos:
-    print u
+	ntags[u] = set()
+
+# El segundo para llenar cada TAG con sus attrs
+for hijo in master:
+	for llave in hijo.attrs.keys():
+		ntags[hijo.name].add(llave)
+
+
+# Ahora solo lo imprimimos bonito
+for llave in ntags.keys():
+	print llave
+	for att in ntags[llave]:
+		print '\t', att
+	if len(ntags[llave]) > 0: print
